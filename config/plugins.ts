584255e88ffb92aa2c0cc2abfc87e967ea52f29a
doodoji <doodozik@gmail.com>
2023-08-07 04:09:31 +0900

    add gcp storage configs

---
+++ config/plugins.ts
@@ -0,0 +1,15 @@
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
+      providerOptions: {
+        serviceAccount: env.json("GCS_SERVICE_ACCOUNT"),
+        bucketName: env("GCS_BUCKET_NAME"),
+        basePath: env("GCS_BASE_PATH"),
+        baseUrl: env("GCS_BASE_PATH"),
+        publicFiles: env("GCS_PUBLIC_FILES"),
+        uniform: env("GCS_UNIFORM"),
+      },
+    },
+  },
+});
