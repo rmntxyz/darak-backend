f3c3bd42cf242a04c7d6ca81385ab522f1a3f077
doodoji <doodozik@gmail.com>
2023-08-07 05:29:12 +0900

    edit server config

--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,10 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  proxy: true,
+  url: env("MY_HEROKU_URL"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+  // webhooks: {
+  //   populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  // },
 });
