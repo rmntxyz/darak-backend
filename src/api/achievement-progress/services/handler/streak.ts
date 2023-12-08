ee7e5695111de88639624c171743701e50f4ec28
doodoji <doodozik@gmail.com>
2023-12-09 03:02:52 +0900

    fix completion_date for streak

--- src/api/achievement-progress/services/handler/streak.ts
+++ src/api/achievement-progress/services/handler/streak.ts
@@ -1,11 +1,41 @@
+import { getRefTimestamp } from "../../../../utils";
 import { simpleProgressOptions } from "../achievement-progress";
 
 async function verify(user: User, progress: AchievementProgress) {
   const { longest_draw } = user.streak;
-  const now = new Date();
 
   const updatedProgresses = [];
   const { milestone_progresses } = progress;
+
+  const completed = await strapi.entityService.findMany(
+    "api::daily-quest-progress.daily-quest-progress",
+    {
+      fields: ["completed_date"],
+      filters: {
+        users_permissions_user: { id: user.id },
+        is_completed: true,
+        daily_quest: { id: 2 },
+      },
+    }
+  );
+
+  const streakHistory = [];
+  let streakCount = 1;
+
+  for (let i = 0; i < completed.length; i++) {
+    const current = completed[i];
+    const next = completed[i + 1];
+
+    const currStamp = getRefTimestamp(current.completed_date);
+    const nextStamp = next ? getRefTimestamp(next.completed_date) : null;
+
+    if (nextStamp && nextStamp - currStamp === 86400000) {
+      streakCount++;
+    } else {
+      streakHistory.push({ start_idx: i - streakCount + 1, streakCount });
+      streakCount = 1;
+    }
+  }
 
   for (const sub of milestone_progresses) {
     if (sub.completed) {
@@ -16,6 +46,14 @@ async function verify(user: User, progress: AchievementProgress) {
     const { progress: subProgress } = sub;
 
     if (longest_draw >= goal) {
+      // find streak range on streakHistory
+      const range =
+        streakHistory.find((range) => range.streakCount >= goal) ||
+        streakHistory[0];
+
+      const { start_idx } = range;
+      const { completed_date } = completed[start_idx + goal - 1];
+
       const updated = await strapi.entityService.update(
         "api::achievement-progress.achievement-progress",
         sub.id,
@@ -24,7 +62,7 @@ async function verify(user: User, progress: AchievementProgress) {
           data: {
             progress: goal,
             completed: true,
-            completion_date: now,
+            completion_date: completed_date,
           },
         }
       );
@@ -47,6 +85,14 @@ async function verify(user: User, progress: AchievementProgress) {
   const { progress: currentProgress } = progress;
 
   if (goal && longest_draw >= goal) {
+    // find streak range on streakHistory
+    const range =
+      streakHistory.find((range) => range.streakCount >= goal) ||
+      streakHistory[0];
+
+    const { start_idx } = range;
+    const { completed_date } = completed[start_idx + goal - 1];
+
     // completed
     const updated = await strapi.entityService.update(
       "api::achievement-progress.achievement-progress",
@@ -56,7 +102,7 @@ async function verify(user: User, progress: AchievementProgress) {
         data: {
           progress: goal,
           completed: true,
-          completion_date: now,
+          completion_date: completed_date,
         },
       }
     );
